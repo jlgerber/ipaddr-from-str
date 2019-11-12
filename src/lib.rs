da1278cb84bb22e27999dcae1f219fd2056c85d8
jgerber <jlgerber@gmail.com>
2019-11-12 06:20:09 -0800

    adds help

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,47 @@ use std::str::FromStr;
 pub mod errors;
 pub use errors::IpaddrConversionError;
 
+/// Retrieve an IpAddr given a &str
+///
+/// # Parameters
+///
+/// * `hostname_or_address` - a str reference which may either be a hostname or an
+/// ipv4 address
+///
+/// # Returns
+/// May either return an IpAddr enum or an IpaddrConversionError
+///
+/// # Example
+///
+/// ```
+/// use ipaddr_from_str;
+/// let result = ipaddr_from_str::get_ipaddr("0.0.0.0");
+/// assert!(result.is_ok());
+/// ```
+pub fn get_ipaddr(hostname_or_address: &str) -> Result<Vec<IpAddr>, IpaddrConversionError> {
+    if is_ipaddrv4(hostname_or_address) {
+        Ok(vec![IpAddr::from_str(hostname_or_address)?])
+    } else {
+        Ok(lookup_host(hostname_or_address)?)
+    }
+}
+
 /// Test if input is an ipaddr v4
 ///
+/// # Arguments
+///
+/// * `input` - the candidate ipv4 address which we will test against a regex to
+/// determine whether it is in the form of an ipv4 address or not.
+///
+/// # Returns
+/// bool
+///
+/// # Example
+/// ```
+/// use ipaddr_from_str;
+/// let result = ipaddr_from_str::is_ipaddrv4("0.0.0.0");
+/// assert!(result);
+/// ```
 pub fn is_ipaddrv4(input: &str) -> bool {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$").unwrap();
@@ -20,15 +59,6 @@ pub fn is_ipaddrv4(input: &str) -> bool {
             .all(|v| v.is_some())
     } else {
         false
-    }
-}
-
-/// Retrieve an address...
-pub fn get_ipaddr(hostname: &str) -> Result<Vec<IpAddr>, IpaddrConversionError> {
-    if is_ipaddrv4(hostname) {
-        Ok(vec![IpAddr::from_str(hostname)?])
-    } else {
-        Ok(lookup_host(hostname)?)
     }
 }
 
